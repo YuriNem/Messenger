76d41b81415eb79832d69db0004d8e472e06ab0a
YuriNem <PsychoactivePie@gmail.com>
2019-03-22 07:48:11 +0300

    Added Express

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
-const http = require('http');
+const express = require('express');
 const fs = require('fs');
 const util = require('util');
 
+const app = express();
 const readFilePromise = util.promisify(fs.readFile);
 
-http.createServer( async (req, res) => {
-    if (req.url === '/') {
-        const html = await readFilePromise('./public/index.html');
-        res.end(html);
-    } else if (req.url.match(/.js$/)) {
-        const js = await readFilePromise('./dist/main.js');
-        res.end(js);
-    }
-}).listen(process.env.PORT || 3000, () => console.log('Server is working'));
+app.get('/', async (req, res) => {
+    const html = await readFilePromise('./public/index.html');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+});
 
+app.get('/dist/main.js', async (req, res) => {
+    const js = await readFilePromise('./dist/main.js');
+    res.writeHead(200, { 'Content-Type': 'text/javascript' });
+    res.end(js);
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is working'));
